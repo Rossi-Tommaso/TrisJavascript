77aa4c7b3b36458cc3d2bfc6b832bb65aa9c1a91
Rossi-Tommaso <tommasoemanuele.rossi@istitutoagnelli.it>
2024-09-19 21:17:04 +0200

    modified:   main.html 	modified:   main.js

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-document.querySelectorAll('.clickable-div').forEach((element) => {
-    element.addEventListener('click', () => console.log('cliccato', element.id));
+let tabella = document.querySelectorAll('.clickable-div');
+let giocatore = true;
+tabella.forEach((element) => {
+    let value = true;
 });
+
+tabella.forEach((element) => {
+    element.addEventListener('click', () => {
+        
+
+        console.log('cliccato', element.id)   
+        if(giocatore){
+            //crea span icona cerchio
+        } else {
+            //crea span icona croce
+        }
+
+        
+    });
+});
+
+console.log(tabella[0].value);
 
 
 
