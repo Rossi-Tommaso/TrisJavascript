b1aa335bdf35794c7dcd90d0583da34868a2254a
Rossi-Tommaso <tommasoemanuele.rossi@istitutoagnelli.it>
2024-09-20 19:02:54 +0200

    -Completate logiche di vittoria 	modified:   home.css 	modified:   main.html 	modified:   main.js 	modified:   style.css

--- main.js
+++ main.js
@@ -1,25 +1,49 @@
-let tabella = document.querySelectorAll('.clickable-div');
-let giocatore = true;
-tabella.forEach((element) => {
-    let value = true;
-});
+let tabella = Array.from(document.querySelectorAll('.item'));
+let giocatore = true; 
+let statoGioco = [null, null, null, null, null, null, null, null, null];
+let statoPartita = true;
 
-tabella.forEach((element) => {
-    element.addEventListener('click', () => {
-        
+const VITTORIA = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
-        console.log('cliccato', element.id)   
-        if(giocatore){
-            //crea span icona cerchio
-        } else {
-            //crea span icona croce
+function Win() {
+    VITTORIA.forEach((condizione) => {
+        let[a, b, c] = condizione;
+        if(statoGioco[a] && statoGioco[a] === statoGioco[b] && statoGioco[a] === statoGioco[c]) {
+            //il giocatore statoGioco[a] ha vinto
+            //aggiungere messaggio di vittoria
+            alert(`il giocatore ${statoGioco[a]} ha vinto`)
+            statoPartita = false;
+            return
         }
 
+        if (!statoGioco.includes(null)) { //se la tabella non include null (cioé é completa) allora è un pareggio
+            statoPartita = false;
+        }
+    })
+}
+
+tabella.forEach((cella, index) => {
+    cella.addEventListener('click', () => {
+      if (!statoGioco[index] && statoPartita) {
+        statoGioco[index] = giocatore ? 'X' : 'O';
+        cella.textContent = giocatore ? 'X' : 'O';
         
+        Win();
+        
+        giocatore = !giocatore;
+      }
     });
-});
+  });
 
-console.log(tabella[0].value);
-
-
+console.log(tabella);
+console.log(!null)
 
